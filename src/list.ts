53798ea4f36d6db62cd554a05741312ec904b734
cybrd <brysuralta@gmail.com>
2024-10-20 02:38:07 +0800

    init

---
+++ src/list.ts
@@ -0,0 +1,46 @@
+import {
+  ChannelType,
+  Client,
+  Collection,
+  GatewayIntentBits,
+  GuildMember,
+} from "discord.js";
+import { config } from "dotenv";
+import moment from "moment";
+config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log("ready");
+});
+
+client.on("messageCreate", (message) => {
+  if (
+    message.content === "!online" &&
+    ["cybrd", ".josap", "ultradash"].find((x) => x === message.author.username)
+  ) {
+    const voiceChannel = message.guild?.channels.cache.get(message.channelId);
+
+    if (voiceChannel?.type === ChannelType.GuildVoice) {
+      const members = voiceChannel?.members as Collection<string, GuildMember>;
+
+      const nameList = members.map((x) => {
+        return x.nickname || x.user.globalName || x.user.username;
+      });
+
+      message.reply(nameList.join("\n"));
+    }
+  }
+});
+
+client.login(process.env.TOKEN);
